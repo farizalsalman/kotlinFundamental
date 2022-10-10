3425344147fb3dc26f9548f00cf2ebac6f3666c9
farizalsalman <fhrzlsalman@gmail.com>
2022-10-10 14:52:22 +0700

    commit numbers2

--- src/main/kotlin/Numbers.kt
+++ src/main/kotlin/Numbers.kt
@@ -5,7 +5,7 @@ fun main() {
     val s: Short = 10
     val b: Byte = Byte.MAX_VALUE
 
-    println("Int value is " + a)
+    println("Nilai integer adalah " + a)
     println("Double value is " + d)
     println("Float value is " + f)
     println("Short value is " + s)
